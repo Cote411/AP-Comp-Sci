ee376e0de4c2e688ebfa0e38a9adf9ec21401869
Alex Cote <apc51104@gmail.com>
2021-09-17 14:09:46 -0400

    instructions

---
+++ Goldbach/Main.java
@@ -0,0 +1,72 @@
+/**
+ *  Class to test Goldbach's Conjecture.
+ *
+ *  Sample methods have been provided for you as a starting point.
+ *  You should, at minimum, implement those methods and the
+ *  main method. You may choose to make these methods static
+ *  (as currently written) or not static.
+ *
+ *  See the description on Classroom for more information.
+ */
+public class Main {
+    /**
+     *  Return a String describing how the given number n can be
+     *  expressed as the sum of primes.
+     *  
+     *  The String should be of the form
+     *     "number = prime1 + prime2"
+     *  where number is equal to n, and prime1 and prime2 are two 
+     *  prime integers that sum to n. The primes can appear in any
+     *  order. 
+     *  If no such primes exist, the method should return null.
+     *  
+     *  Precondition: n is an even integer greater than 2
+     *
+     *  @param n an even integer greater than 2
+     *  @return String describing the prime sum, or null
+     */
+    public static String getPrimeSum(int n) {
+      // TODO: write this method
+      return null;
+    }
+  
+    /**
+     *  Test Goldbach's Conjecture for all even integers greater than
+     *  2 and less than or equal to the specified value.
+     *  
+     *  For integers that pass the test, the method should output
+     *  a single line showing how the integer can be expressed as
+     *  the sum of primes, in the same format as the strings
+     *  returned from the getPrimeSum method.
+     *  
+     *  For integers that fail the test, the method should output
+     *  a line that says "n fails the test", where n is the number.
+     *  If Goldbach is correct, this shouldn't happen.
+     *
+     *  The method should return true if all integers pass,
+     *  and false otherwise.
+     *  
+     *  Precondition: max is greater than 2
+     *
+     *  @param max an integer greater than 2
+     *  @return true if and only if all integers pass
+     */
+    public static boolean testGoldbach(int max) {
+      // TODO: write this method
+      return false;
+    }
+  
+    /**
+     * Prompt the user for the maximum integer.
+     * If the integer is 2 or less, display the error message
+     * "Invalid integer" and exit.
+     * Otherwise, use your testGoldbach method to test all integers
+     * up to and including that maximum integer.
+     * If testGoldbach returns false and Goldbach was wrong,
+     * your program should print
+     * "Goldbach's conjecture has been disproven."
+     */
+    public static void main(String[] args) {
+      // TODO: write this method
+    }
+  }
